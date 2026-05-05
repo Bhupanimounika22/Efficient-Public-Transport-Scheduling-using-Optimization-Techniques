67a78b5147fdc69b7cc5dc29d67c8b52ec63c24b
Mounika <127584838+Bhupanimounika22@users.noreply.github.com>
2026-05-05 14:41:08 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Mounika@2008",
+        password="",
         database="transport_db"
     )
